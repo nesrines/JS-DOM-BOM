117a04a20186e526ba52305c70ecceb0f598e9e1
Nasrin F. Asadli <nasrinfa@code.local>
2023-06-19 17:24:34 +0400

    fifth

--- Task-2/js/app.js
+++ Task-2/js/app.js
@@ -1,6 +1,11 @@
 document.getElementById("Search").addEventListener("click", () => {
     let SearchBar = document.getElementById("SearchBar");
     SearchBar.classList.toggle("d-none");
+    SearchBar.classList.toggle("d-flex");
     document.getElementById("Search").classList.toggle("active");
-    
+})
+document.querySelector(".fa-xmark").addEventListener("click", () => {
+    let SearchBar = document.getElementById("SearchBar");
+    SearchBar.classList.replace("d-flex", "d-none");
+    document.getElementById("Search").classList.remove("active");
 })
